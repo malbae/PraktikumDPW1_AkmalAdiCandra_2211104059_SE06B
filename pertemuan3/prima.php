0a3968b71d97e48831e0c68768b7202547ab6fa5
malbae <akmal67e@gmail.com>
2024-03-25 16:06:20 +0700

    pertemuan3

---
+++ pertemuan3/prima.php
@@ -0,0 +1,21 @@
+<?php
+function isPrime($num)
+{
+    if ($num <= 1) {
+        return false;
+    }
+    for ($i = 2; $i <= sqrt($num); $i++) {
+        if ($num % $i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+echo "Bilangan prima dari 1 sampai 100 adalah: <br>";
+for ($i = 1; $i <= 100; $i++) {
+    if (isPrime($i)) {
+        echo $i . " adalah bilangan prima <br>";
+    }
+}
+?>
